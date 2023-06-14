8bb6d7d60045c18104c82174dc51c4c90d28acab
Vaibhav Kharche <vaibhavmkharche@gmail.com>
2023-06-14 16:45:09 +0530

    Initial Commit

---
+++ src/main/java/com/maveric/realtimedatapoc/RealtimedatapocApplication.java
@@ -0,0 +1,13 @@
+package com.maveric.realtimedatapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimedatapocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimedatapocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maveric/realtimedatapoc/RealtimedatapocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.realtimedatapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimedatapocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
